74d43d8366d42b45365daf3dc3915059ade95afa
Carmen Cecilia Gomez Carmona <carmgome@c2r1s3.42malaga.com>
2026-07-23 17:22:16 +0200

    changes

--- ex0/ft_command_quest.py
+++ ex0/ft_command_quest.py
@@ -1,11 +1,12 @@
 import sys
 
-print("=== Command Quest ===")
-print("Program name:", sys.argv[0])
-if len(sys.argv) == 1:
-    print("No arguments provided!")
-else:
-    print("Arguments received:", len(sys.argv) - 1)
-    for i in range(1, len(sys.argv)):
-        print(f"Argument {i}: {sys.argv[i]}")
-print("Total arguments:", len(sys.argv))
+if __name__ == "__main__":
+    print("=== Command Quest ===")
+    print("Program name:", sys.argv[0])
+    if len(sys.argv) == 1:
+        print("No arguments provided!")
+    else:
+        print("Arguments received:", len(sys.argv) - 1)
+        for i in range(1, len(sys.argv)):
+            print(f"Argument {i}: {sys.argv[i]}")
+    print("Total arguments:", len(sys.argv))
--- ex1/ft_score_analytics.py
+++ ex1/ft_score_analytics.py
@@ -1,4 +1,32 @@
 import sys
 
-print("=== Player Score Analytics ===")
-print("Scores processed:")
+
+def check_arguments(argv: list[str]) -> list[int]:
+    print("=== Player Score Analytics ===")
+    valid_scores = []
+    for i in range(1, len(argv)):
+        try:
+            valid_scores.append(int(argv[i]))
+        except ValueError:
+            print(f"Invalid parameter: '{argv[i]}'")
+    if len(valid_scores) < 1:
+        print("No scores provided. Usage: python3 "
+              "ft_score_analytics.py <score1> <score2> ... ")
+    else:
+        total_players = len(valid_scores)
+        total_score = sum(valid_scores)
+        average_score = total_score / total_players
+        high_score = max(valid_scores)
+        low_score = min(valid_scores)
+        score_range = high_score - low_score
+        print("Scores processed:", valid_scores)
+        print("Total players:", total_players)
+        print("Total score:", total_score)
+        print("Average score:", average_score)
+        print("High score:", high_score)
+        print("Low score:", low_score)
+        print("Score range:", score_range)
+    return valid_scores
+
+
+check_arguments(sys.argv)
