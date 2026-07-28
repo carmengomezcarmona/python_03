318f352cf77f9f0824549ffc356bde0f8d81462d
Carmen Cecilia Gomez Carmona <carmengomezcarmona@gmail.com>
2026-07-28 19:42:31 +0200

    changes

--- ex3/ft_achievement_tracker.py
+++ ex3/ft_achievement_tracker.py
@@ -1,8 +1,6 @@
 import random
 
-
-def gen_player_achievements() -> set:
-    all_achievements = [
+all_achievements = [
         "Crafting Genius",
         "World Savior",
         "Untouchable",
@@ -17,11 +15,35 @@ def gen_player_achievements() -> set:
         "Collector Supreme",
         "Sharp Mind"
     ]
-    number = random.randint(1, 4)
+
+
+def gen_player_achievements() -> set[str]:
+    number = random.randint(3, 8)
     return set(random.sample(all_achievements, number))
 
 
 if __name__ == "__main__":
     print("=== Achievement Tracker System ===")
-    print("Player Alice: ", gen_player_achievements())
-    
+    alice = gen_player_achievements()
+    bob = gen_player_achievements()
+    charlie = gen_player_achievements()
+    dylan = gen_player_achievements()
+    print("Player Alice: ", alice)
+    print("Player Bob: ", bob)
+    print("Player Charlie: ", charlie)
+    print("Player Dylan: ", dylan)
+    print()
+    print("All distinct achievements: ", alice.union(bob, charlie, dylan))
+    print()
+    print("Common achievements: ", alice.intersection(bob, charlie, dylan))
+    print()
+    print("Only Alice has: ", alice.difference(bob, charlie, dylan))
+    print("Only Bob has: ", bob.difference(alice, charlie, dylan))
+    print("Only Charlie has: ", charlie.difference(alice, bob, dylan))
+    print("Only Dylan has: ", dylan.difference(alice, bob, charlie))
+    print()
+    missing = set(all_achievements)
+    print("Alice is missing: ", missing.difference(alice))
+    print("Bob is missing: ", missing.difference(bob))
+    print("Charlie is missing: ", missing.difference(charlie))
+    print("Dylan is missing: ", missing.difference(dylan))
